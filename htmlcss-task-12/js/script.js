50597083ccfa9ff0db4898925c0a8e93facc5f84
Anastasia Khmara <zavetnaya.13@gmail.com>
2019-10-31 22:46:57 +0300

    complete htmlcss-task-12

--- htmlcss-task-12/js/script.js
+++ htmlcss-task-12/js/script.js
@@ -1,20 +1,52 @@
-function searchValue() {
+// Реализуйте функцию поиска с добавлением параметра по умолчанию:
 
-    let val = document.querySelector('search').value.trim();
-    let searchItem = document.querySelectorAll('.list tr');
+// создайте в хедере страницы поле для поиска и кнопку, по нажатии на которую начинается поиск. 
 
-    if (val != '') {
-        searchItem.forEach(function(elem) {
-            if (elem.innerText.search(val) == -1) {
-                elem.classList.add('hide');
-            } else {
-                elem.classList.remove('hide');
-                alert('Ничего не найдено!');
-            }
-        });
-    } else {
-        searchItem.forEach(function(elem) {
-            elem.classList.remove('hide');
-        });
+// реализуйте поиск объектов. При запуске поиска с пустым полем поиск должен вывести сообщение "Ничего не найдено!".
+
+// реализуйте отображение результатов поиска на странице 
+
+const searchHundler = () => {
+
+    const $searchvalue = document.getElementById('search-value');
+    const $inputvalue = document.getElementById('search-item');
+    const $tbody = document.getElementById('body-item');
+    const $tr = $tbody.children;
+
+    const $valueinputs = $searchvalue.children;
+
+    let arrformdata = [];
+
+    for (let input of $valueinputs) {
+        arrformdata.push(input.value.toUpperCase());
+    }
+
+    let arrtrdata = [];
+
+    for (let value of $tr) {
+
+        const item = value;
+        const td = value.children;
+
+        for (let value of td) {
+            arrtrdata.push(value.textContent);
+        }
+
+        let tdarray = [td[0].textContent.toUpperCase(), td[1].textContent.toUpperCase(), td[2].textContent.toUpperCase(), td[3].textContent.toUpperCase()];
+
+        let status = tdarray.includes(arrformdata[0]);
+
+        console.log(status);
+        console.log(item);
+
+        if (status == true) {
+            item.style.display = 'table-row';
+        } else {
+            item.style.display = 'none';
+        }
+    }
+
+    if ($inputvalue.value == "") {
+        document.getElementById('body-item').innerHTML = '<tr><td class="text" colspan="4">Ничего не найдено!</td></tr>';
     }
 }
